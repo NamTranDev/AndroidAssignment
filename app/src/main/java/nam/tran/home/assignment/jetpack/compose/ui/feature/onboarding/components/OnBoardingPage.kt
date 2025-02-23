d40ac217be86f2e740c13a3c710a89cc004abc95
Nam nam <namtran09061992@gmail.com>
2025-02-23 22:29:54 +0700

    Implement Boarding UI

---
+++ app/src/main/java/nam/tran/home/assignment/jetpack/compose/ui/feature/onboarding/components/OnBoardingPage.kt
@@ -0,0 +1,15 @@
+package nam.tran.home.assignment.jetpack.compose.ui.feature.onboarding.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OnBoardingPage(
+    @DrawableRes image : Int,
+    title : String,
+    description : String,
+    modifier: Modifier = Modifier
+){
+
+}
---
+++ app/src/main/java/nam/tran/home/assignment/jetpack/compose/ui/feature/onboarding/screen/OnBoardingScreen.kt
@@ -0,0 +1,6 @@
+package nam.tran.home.assignment.jetpack.compose.ui.feature.onboarding.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OnBoardingScreen(){}
