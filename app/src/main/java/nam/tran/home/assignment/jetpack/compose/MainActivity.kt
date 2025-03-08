b0f863092cc995ff3d842ac73fe768f50f6bc669
Nam nam <namtran09061992@gmail.com>
2025-03-08 23:04:14 +0700

    - Complete Boarding UI

--- app/src/main/java/nam/tran/home/assignment/jetpack/compose/MainActivity.kt
+++ app/src/main/java/nam/tran/home/assignment/jetpack/compose/MainActivity.kt
@@ -1,14 +1,19 @@
 package nam.tran.home.assignment.jetpack.compose
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,18 +25,23 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
+import nam.tran.home.assignment.jetpack.compose.ui.feature.onboarding.screen.OnBoardingScreen
 import nam.tran.home.assignment.jetpack.compose.ui.theme.JetpackComposeHomeAssignmentTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        actionBar?.hide()
         enableEdgeToEdge()
         installSplashScreen()
         setContent {
             JetpackComposeHomeAssignmentTheme {
-
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)){
+                    OnBoardingScreen()
+                }
             }
         }
     }
--- app/src/main/java/nam/tran/home/assignment/jetpack/compose/ui/feature/onboarding/screen/OnBoardingScreen.kt
+++ app/src/main/java/nam/tran/home/assignment/jetpack/compose/ui/feature/onboarding/screen/OnBoardingScreen.kt
@@ -1,6 +1,93 @@
 package nam.tran.home.assignment.jetpack.compose.ui.feature.onboarding.screen
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
+import nam.tran.home.assignment.jetpack.compose.ui.Dimens.mediumPadding
+import nam.tran.home.assignment.jetpack.compose.ui.Dimens.pageIndicatorWidth
+import nam.tran.home.assignment.jetpack.compose.ui.Dimens.spaceHeight
+import nam.tran.home.assignment.jetpack.compose.ui.common.ButtonApp
+import nam.tran.home.assignment.jetpack.compose.ui.feature.onboarding.components.OnBoardingPage
+import nam.tran.home.assignment.jetpack.compose.ui.feature.onboarding.components.PageIndicator
+import nam.tran.home.assignment.jetpack.compose.ui.feature.onboarding.model.PageInfo.Companion.pageInfos
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingScreen(){}
+fun OnBoardingScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        val pageState = rememberPagerState(initialPage = 0) {
+            pageInfos.size
+        }
+
+        val buttonState = remember {
+            derivedStateOf {
+                if (pageState.currentPage == 0) {
+                    listOf("", "Next")
+                } else if (pageState.currentPage == pageState.pageCount - 1) {
+                    listOf("Back", "Get Started")
+                } else {
+                    listOf("Back", "Next")
+                }
+            }
+        }
+
+        HorizontalPager(pageState) { index ->
+            OnBoardingPage(pageInfos.get(index))
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = mediumPadding)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            PageIndicator(
+                modifier = Modifier.width(pageIndicatorWidth),
+                pageSize = pageState.pageCount,
+                selectedPage = pageState.currentPage
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val scope = rememberCoroutineScope()
+                if (buttonState.value[0].isNotEmpty()) {
+                    ButtonApp(onClick = {
+                        scope.launch {
+                            pageState.animateScrollToPage(page = pageState.currentPage - 1)
+                        }
+                    }, text = buttonState.value[0], isTextButton = true)
+                }
+                ButtonApp(onClick = {
+                    scope.launch {
+                        if (pageState.currentPage == pageState.pageCount - 1) {
+                            //go to home page
+                        } else {
+                            pageState.animateScrollToPage(page = pageState.currentPage + 1)
+                        }
+                    }
+                }, text = buttonState.value[1])
+            }
+        }
+        Spacer(modifier = Modifier.height(spaceHeight))
+    }
+}
